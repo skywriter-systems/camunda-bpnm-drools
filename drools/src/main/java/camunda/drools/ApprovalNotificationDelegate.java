18c3f727178441604acdda65fd80581962cae441
echasin <echasin@innvosolutions.com>
2019-09-26 10:09:41 -0700

    WIP - outputting RejectNotifcationDelegate

---
+++ drools/src/main/java/camunda/drools/ApprovalNotificationDelegate.java
@@ -0,0 +1,23 @@
+package camunda.drools;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+/**
+ * Rejection is just done via a sysout since the fox platform does not support the Mail Task of Activiti!
+ * See https://app.camunda.com/confluence/display/foxUserGuide/Activiti+5+Support for details.
+ * 
+ * Use your own Mail mechanisms for this or use your application server features. 
+ */
+public class ApprovalNotificationDelegate implements JavaDelegate {
+  
+  public void execute(DelegateExecution execution) throws Exception {
+    Tweet tweet = (Tweet) execution.getVariable("tweet");
+    
+    System.out.println("Hi!\n\n"
+           + "Your Tweet has been approved.\n\n"
+           + "Original content: " + tweet.getContent() + "\n\n"
+           );
+  }
+  
+}
---
+++ drools/src/main/java/camunda/drools/ProcessVariablesDelegate.java
@@ -0,0 +1,21 @@
+package camunda.drools;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class ProcessVariablesDelegate implements JavaDelegate {
+
+	public void execute(DelegateExecution execution) throws Exception {
+		System.out.println("Process Variable ...");
+		execution.setVariable("author","echasin");
+		execution.setVariable("email","echasin@innvosolutions.com");
+		execution.setVariable("content","This is demo tweet-John Doe");
+		
+		String author = (String) execution.getVariable("author");
+		String email = (String) execution.getVariable("email");
+		String content = (String) execution.getVariable("content");
+		
+		System.out.println("ProcesVariablesDelegate-Process variables: " + author + " - " + email + " - " + content);
+
+	}
+}
